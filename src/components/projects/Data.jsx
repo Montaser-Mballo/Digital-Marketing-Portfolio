dc49ec1c72aa1e678a2ae375125fbd1fd3f5995c
Montaser-Mballo <montaser.mballo@gmail.com>
2024-10-18 17:43:18 +0400

    Initial commit after resetting

---
+++ src/components/projects/Data.jsx
@@ -0,0 +1,59 @@
+import Project1 from "../../assets/Habeebi Academy logo.jpg";
+import Project2 from "../../assets/links-corporate-consultants.jpg";
+import Project3 from "../../assets/al-jubail-city.jpg";
+import Project4 from "../../assets/Habeebi Academy logo.jpg";
+import Project5 from "../../assets/fresho-supermarket-logo.png";
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Project1,
+        title: "SEO for an education platform",
+        category: "SEO",
+        link: "/project/1",
+    },
+    {
+        id: 2,
+        image: Project4,
+        title: "Competitor Analysis",
+        category: "All",
+        link: "/project/2", 
+    },
+    {
+        id: 3,
+        image: Project2,
+        title: "Social media",
+        category: "Content Creation",
+        link: "/project/3", 
+    },
+    {
+        id: 4,
+        image: Project5,
+        title: "Social media",
+        category: "Content Creation",
+        link: "/project/4", 
+    },
+    {
+        id: 5,
+        image: Project3,
+        title: "City branding of Al Jubail",
+        category: "Branding",
+        link: "/project/5", 
+    },
+];
+
+
+export const projectsNav = [
+    {
+        name: "All",
+    },
+    {
+        name: "SEO",
+    },
+    {
+        name: "Content Creation",
+    },
+    {
+        name: "Branding",
+    },
+];
---
+++ src/pages/project_3/Project_Page_3.jsx
@@ -0,0 +1,272 @@
+import "../project_page.css";
+import { useEffect } from "react";
+import { HiOutlineArrowSmRight } from "react-icons/hi";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+import ProjectImg from "../../assets/links-corporate-consultants.jpg";
+import Canva from "../../assets/canva icon.png";
+import Hootsuite from "../../assets/hootsuite icon.png";
+import Slack from "../../assets/slack-icon.png";
+import GoogleAnalytics from "../../assets/google analytics logo.png";
+import PersonIcon from "../../assets/person-icon.png";
+import AnalyticsIcon from "../../assets/analytic-icon.png";
+import MediaIcon from "../../assets/media-icon.png";
+
+const Project_Page_3 = () => {
+  // Scroll to the top when the page loads
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="project__page">
+      <Navbar />
+
+      <div className="project-header">
+        <h1>Social Media</h1>
+        <p className="project-subtitle">Social media strategy</p>
+      </div>
+
+      <div className="project_intro">
+        <div className="project_img_container">
+          <div>
+            <img src={ProjectImg} alt="links-corporate-consultants-logo" />
+          </div>
+        </div>
+
+        <div className="project_overview">
+          <h2>Description</h2>
+
+          <p>
+            Developed and executed comprehensive social media content strategies
+            focusing on engaging visuals and videos to enhance brand visibility
+            and audience engagement on platforms like Instagram and YouTube.
+          </p>
+
+          <h2>Key Skills Used</h2>
+
+          <p>
+            Content creation, graphic design, video editing, social media
+            strategy, project management.
+          </p>
+
+          <h2>Technologies Used</h2>
+
+          <div className="tools_icons">
+            <div className="icon_item">
+              <img src={Canva} alt="Canva Icon" />
+              <p>Canva</p>
+            </div>
+
+            <div className="icon_item">
+              <img src={Hootsuite} alt="Hootsuite Icon" />
+              <p>Hootsuite</p>
+            </div>
+
+            <div className="icon_item">
+              <img src={Slack} alt="Slack Icon" />
+              <p>Slack</p>
+            </div>
+
+            <div className="icon_item">
+              <img src={GoogleAnalytics} alt="Google Analytics Icon" />
+              <p>Google Analytics</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="project_body">
+        <div className="project_card">
+          <img src={PersonIcon} alt="Person Icon" />
+
+          <h2>Role</h2>
+
+          <p>
+            <strong>In my role, I was responsible for:</strong>
+          </p>
+
+          <article>
+            <ul>
+              <li>
+                Creating a content calendar to plan and organize posts,
+                designing eye-catching posters, and producing short videos
+                tailored for Instagram Reels and YouTube. This involved
+                brainstorming creative concepts, collaborating with the clients
+                to align with their brand identity, and ensuring timely delivery
+                of all content.
+              </li>
+            </ul>
+          </article>
+        </div>
+
+        <div className="project_card">
+          <img src={AnalyticsIcon} alt="Analytics Icon" />
+
+          <h2>Outcome</h2>
+
+          <p>
+            <strong>The project resulted in:</strong>
+          </p>
+
+          <article>
+            <ul>
+              <li>
+                Successfully increased audience engagement across all platforms,
+                with measurable improvements such as increased likes, shares,
+                and comments. The content resonated well with target
+                demographics, leading to an uptick in follower growth for the
+                company.
+              </li>
+            </ul>
+          </article>
+        </div>
+
+        <div className="project_card">
+          <img src={MediaIcon} alt="Media Icon" />
+
+          <h2>Media</h2>
+
+          {/* <p>
+            <strong>Some Posters I Made on Instagram:</strong>
+          </p>
+
+          <article>
+            <div>
+              <a
+                href="https://www.instagram.com/p/DADzUL1p8wR/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Post 1{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+
+              <a
+                href="https://www.instagram.com/p/DBOgo80yOy7/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Post 2{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+
+              <a
+                href="https://www.instagram.com/p/DA6MLoHoQfr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Post 3{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+            </div>
+          </article>
+
+          <p>
+            <strong>Some Videos I Made on Instagram:</strong>
+          </p>
+
+          <article>
+            <div>
+              <a
+                href="https://www.instagram.com/reel/C0MZJK-rAo6/?utm_source=ig_web_copy_link"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Video 1{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+
+              <a
+                href="https://drive.google.com/drive/folders/1hguI_FdW4P20xOQZYCOrDdRLE7Ec7sHS?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Video 2{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+
+              <a
+                href="https://drive.google.com/drive/folders/1hguI_FdW4P20xOQZYCOrDdRLE7Ec7sHS?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Video 3{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+            </div>
+          </article>
+
+          <p>
+            <strong>Some Videos I Made on YouTube:</strong>
+          </p>
+
+          <article>
+            <div>
+              <a
+                href="https://drive.google.com/drive/folders/1hguI_FdW4P20xOQZYCOrDdRLE7Ec7sHS?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Video 1{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+
+              <a
+                href="https://drive.google.com/drive/folders/1hguI_FdW4P20xOQZYCOrDdRLE7Ec7sHS?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Video 2{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+
+              <a
+                href="https://drive.google.com/drive/folders/1hguI_FdW4P20xOQZYCOrDdRLE7Ec7sHS?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__button">
+                Video 3{" "}
+                <HiOutlineArrowSmRight className="project__button-icon" />
+              </a>
+            </div>
+          </article> */}
+
+          <p>
+            <strong>Project Files</strong>
+          </p>
+
+          <article>
+            <ul>
+              <li>
+                <a
+                  href="https://drive.google.com/drive/folders/1hguI_FdW4P20xOQZYCOrDdRLE7Ec7sHS?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project__button">
+                  Links corporate Consultants{" "}
+                  <HiOutlineArrowSmRight className="project__button-icon" />
+                </a>
+              </li>
+            </ul>
+          </article>
+        </div>
+      </div>
+
+      <div className="btn_container">
+        <div>
+          <a
+            href="//linksuae.ae/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button button--flex">
+            Visit Company Page
+          </a>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Project_Page_3;
